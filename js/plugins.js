f32589550e9bd20d1f547615448341dd9a467e0c
Zeki <zckural@gmail.com>
2021-12-09 16:57:54 -0500

    add photos

--- js/plugins.js
+++ js/plugins.js
@@ -6595,7 +6595,7 @@ particlesJS('particles-js',
 function initMap() {
 
     var map = new google.maps.Map(document.getElementById('ieatmaps'), {
-      center: {lat: 34.0937458, lng: -118.3614978},
+      center: {lat: 40.706324, lng: -74.007808},
       zoom: 12,
       styles: [
     {
@@ -6767,7 +6767,7 @@ function initMap() {
     });
 
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(34.0937458, -118.3614978)
+        position: new google.maps.LatLng(40.706324, -74.007808)
         , title: 'ASL'
         , map : map
 
